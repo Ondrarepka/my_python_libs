855322a8bda70f5747d6a3b907c00f5c421ed5d1
LordRepizz <ondrej.repka@seznam.cz>
2026-01-12 01:09:00 +0100

    Add Good Habits script with main execution block

---
+++ GoodHabits.py
@@ -0,0 +1,41 @@
+# Good Habits
+# https://www.youtube.com/watch?v=I72uD8ED73U
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+## Have this block to ensure the script runs only when executed directly - not when imported as a module.
+# Include any code that should only run when the script is executed directly inside this block. (testing the module etc.)
+
+if __name__ == "__main__":
+    print("This script is being run directly.") 
